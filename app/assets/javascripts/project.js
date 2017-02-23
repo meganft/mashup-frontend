5e55187999913cecd8af05d914c2a6d2618d768f
Megan Talbot <meganftalbot@gmail.com>
2017-02-23 10:29:00 -0700

    Add popsicle view, script tags in html views

--- app/assets/javascripts/project.js
+++ app/assets/javascripts/project.js
@@ -1,21 +1,21 @@
-var API = 'https://mashup-api.herokuapp.com';
-
-$(document).ready(function(){
-  var cohortId = window.location.pathname.split("/")[2];
-  //this gets the cohort id, need the name for below to work
-
-  var getProjects = function() {
-    return $.ajax({
-      url: API + '/api/v1/projects?name=' + cohortId,
-      method: 'GET',
-    }).done(function(data){
-      for (var i = 0; i < data.length; i++) {
-        $('.projects').append('<p class="project">' + data[i].name + '</p>');
-      }
-    }).fail(function(error){
-      console.error(error);
-    });
-  };
-
-  getProjects();
-});
+// var API = 'https://mashup-api.herokuapp.com';
+//
+// $(document).ready(function(){
+//   var cohortId = window.location.pathname.split("/")[2];
+//   //this gets the cohort id, need the name for below to work
+//
+//   var getProjects = function() {
+//     return $.ajax({
+//       url: API + '/api/v1/projects?name=' + cohortId,
+//       method: 'GET',
+//     }).done(function(data){
+//       for (var i = 0; i < data.length; i++) {
+//         $('.projects').append('<p class="project">' + data[i].name + '</p>');
+//       }
+//     }).fail(function(error){
+//       console.error(error);
+//     });
+//   };
+//
+//   getProjects();
+// });
