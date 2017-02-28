eb1b877e69222b1cace598b975f5921f74801508
Jon Kimble <jonathanbkimble@gmail.com>
2017-02-28 14:01:03 -0700

    Finish cohort pairs

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   resources :cohorts, only: :show do
     resources :popsicle, only: [:index]
     resources :projects, only: [:index, :show, :new, :create]
+    resources :pairs, only: [:index]
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
